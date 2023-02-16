5c7a6556b0d4c94679b4d80f217da05c8ceaaf07
ZaraDoe <zaradoehrh@gmail.com>
2023-02-16 15:24:02 -0800

    My first C program

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - C program that prints with proper grammer
+ *
+ * Return: 0 is success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
---
+++ 0x00-hello_world/README.md
@@ -0,0 +1,1 @@
+This file contains Preprocessor Compiler Assembler of my first C programming
